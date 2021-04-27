32a711934e57384380273e02b9d8658c0065a353
Arne Emmel <arne.emmel@kip.uni-heidelberg.de>
2021-04-27 14:09:12 +0000

    Execute tests in correct working directory
    Change-Id: I06309c7ad376f587ad0bb609b72061b1a57b7674

--- tests/shell/test_all_ipynb.sh
+++ tests/shell/test_all_ipynb.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-find "${BLD_DIR}" -name "*.ipynb" -type f -print0 | xargs -0 -I"{}" ipython --colors=NoColor -c\
-  "import IPython; IPython.get_ipython().safe_execfile_ipy('{}', raise_exceptions=True)"
+exec_test() {
+    local testfile
+    testfile="$1"
+    cd "$(dirname "${testfile}")"
+    ipython --colors=NoColor -c\
+        "import IPython; IPython.get_ipython().safe_execfile_ipy('$(basename ${testfile})', raise_exceptions=True)"
+}
+export -f exec_test
+
+find "${BLD_DIR}" -name "*.ipynb" -type f -print0 | xargs -0 -n 1 -I{} bash -c "exec_test '{}'"
